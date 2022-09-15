15aff013b6db5c8763d693f08f2b48766b681020
iepppop <iepppop@gmail.com>
2022-09-15 23:53:56 +0900

    m

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <ul className="nav">
+            <li></li>
+            <li>스토어</li>
+            <li>Mac</li>
+            <li>iPad</li>
+            <li>iPhone</li>
+            <li>Watch</li>
+            <li>TV 및 홈</li>
+            <li>Apple 독점 제공</li>
+            <li>액세서리</li>
+            <li>고객지원</li>
+            <li>ㅇ</li>
+            <li>ㅁ</li>
+        </ul>
+    </div>
+  )
+}
+export default Navbar
